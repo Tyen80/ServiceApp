5975416a92324d175312d966d2f543ba949fb263
Ove Ronny Bolme <overonny@progorb.no>
2024-09-26 21:32:10 +0200

    Added pages for login and register user. Added Bootstrap cdn

---
+++ ServiceApp.UI/ServiceApp.UI/Features/Users/LoginUserModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ServiceApp.UI.Features.Users;
+
+public class LoginUserModel
+{
+    [Required]
+    public string UserName { get; set; } = string.Empty;
+    [Required]
+    public string Password { get; set; } = string.Empty;
+}
---
+++ ServiceApp.UI/ServiceApp.UI/Features/Users/RegisterUserModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ServiceApp.UI.Features.Users;
+
+public class RegisterUserModel
+{
+    [Required]
+    public string UserName { get; set; } = string.Empty;
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+    [Required]
+    public string Password { get; set; } = string.Empty;
+    [Required]
+    [Compare("Password", ErrorMessage = "Password does not match")]
+    public string ConfirmPassword { get; set; } = string.Empty;
+}
